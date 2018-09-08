a896a4b93cad3af9becf352f5609bcf6e090cb57
Catherine Du <yd2386@columbia.edu>
2018-09-08 18:51:34 -0400

    Update visual-search-dal.php

--- visual-search-dal.php
+++ visual-search-dal.php
@@ -3,7 +3,7 @@
 //open a new connection to tagging area
 $host = '64.64.20.231';
 $user = 'gopixel_tg_usr';
-$pass = 'iWQ;uRuZRF$P';
+$pass = 'xxxxxxx';
 $dbname = 'gopixel_tagging_area';
 $conn = new mysqli($host, $user, $pass, $dbname);
 
